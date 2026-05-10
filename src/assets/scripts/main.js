bac06c2cfe73b3593067e7164aed311399f8a7fa
Eric Bleda <ericbledab@educand.ad>
2026-05-10 20:02:21 +0200

    Fix Netlify URL detection

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -11,15 +11,23 @@ import * as bootstrap from 'bootstrap';
  * Write any other JavaScript below
  */
 
-// Actualització de l'enllaç actiu del navbar del header+footer
-console.log("Test")
-const currentPage = window.location.pathname.split('/').pop() || 'index.html';
+// Recupera la pàgina actual i normalitza el nom.
+const currentPath = window.location.pathname;
+const currentPage = currentPath
+  .split('/')
+  .pop()
+  .replace('.html', '') || 'index';
+  
 // Seleccionem tots els enllaços del menú de navegació.
 const navLinks = document.querySelectorAll('.site-header__link');
 
 navLinks.forEach((link) => {
-  // Recuperem el valor de l'atribut href de cada enllaç.
-  const linkPage = link.getAttribute('href');
+  // Recupera l'href del link i també el normalitza.
+  const linkPage = link
+    .getAttribute('href')
+    .split('/')
+    .pop()
+    .replace('.html', '');
 
   // Modifiquem la classe "active" segons si l'enllaç coincideix amb la pàgina actual.
   link.classList.toggle('active', linkPage === currentPage);
